54b8e29e8a4ebcc95499d80113ac304031d01b90
Lelana <mstlelanakhatun@gmail.com>
2023-05-23 16:34:13 -0300

    home commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
     return (
         <div className="">
-            <Banner></Banner>
+            
             <Gallery></Gallery>
             <TabSection></TabSection>
             <HappyCustomer></HappyCustomer>
